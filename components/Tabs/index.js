5832f2da6858516a05aea24491134a05b414e141
Kimberly Boyd <kimberlyboyd@Kimberlys-MacBook-Pro.local>
2019-12-16 19:52:20 -0500

    i think tabs are done

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,26 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+const tab = document.querySelector('.topics');
+
+function tabComponent(data) {
+    const newTopic = document.createElement('div');
+
+    newTopic.classList.add("tab");
+    newTopic.textContent = data;
+
+    return newTopic;
+}
+
+axios
+    .get('https://lambda-times-backend.herokuapp.com/topics')
+    .then((res) => {
+        res.data.topics.forEach(item => {
+            tab.appendChild(tabComponent(item));
+        })
+    })
+    .catch((err) => {
+        console.log('You hit an error', err);
+    });
