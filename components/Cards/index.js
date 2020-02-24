03afe04b4b2968b25868ec2d50b1b708c06f6669
Kimberly Boyd <kimberlyboyd@Kimberlys-MacBook-Pro.local>
2020-02-23 21:19:18 -0500

    finished MVP

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,48 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardContainer = document.querySelector('.cards-container')
+
+    function createCard(arr) {
+
+        const card = document.createElement('div');
+        const headline = document.createElement('div');
+        const author = document.createElement('div');
+        const imageContainer = document.createElement('div');
+        const image = document.createElement('img');
+        const authorCard = document.createElement('span');
+
+        card.classList.add('card');
+        headline.classList.add('headline');
+        author.classList.add('author');
+        imageContainer.classList.add('img-container');
+
+        headline.textContent = arr.headline;
+        image.src = arr.authorPhoto;
+        authorCard.textContent = `By: ${arr.authorName}`;
+
+        card.appendChild(headline);
+        card.appendChild(author);
+        author.appendChild(imageContainer);
+        imageContainer.appendChild(image);
+        author.appendChild(authorCard);
+
+
+        return card;
+
+    }
+
+    axios.get('https://lambda-times-backend.herokuapp.com/articles')
+        .then(response => {
+            console.log(response.data.articles);
+            const articles = response.data.articles;
+            Object.keys(articles).forEach(key => {
+                articles[key].forEach(article => {
+                    cardContainer.appendChild(createCard(article))
+                })
+            });
+        })
+        .catch(error => console.log('Error!', error))
+
+
