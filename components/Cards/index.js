d1dec72926420f84cd10e98d39ac1eea6bf97786
Kimberly Boyd <kimberlyboyd@Kimberlys-MacBook-Pro.local>
2020-04-19 20:03:30 -0400

    finished cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,7 +18,50 @@
 //
 // Create a card for each of the articles and add the card to the DOM.
 
+const cardsContainer = document.querySelector('.cards-container');
 
+axios
+    .get('https://lambda-times-backend.herokuapp.com/articles')
+    .then((res) => {
+        console.log(res.data.articles);
+        
+        const articles = res.data.articles;
+        Object.keys(articles).forEach(key => {
+            articles[key].forEach(article => {
+                cardsContainer.appendChild(createCard(article))
+            })
+        });
+    })
 
+function createCard(data) {
+
+    // create elements
+    const newCard = document.createElement('div');
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+    const imgContain = document.createElement('div');
+    const image = document.createElement('img');
+    const authName = document.createElement('span');
+
+    // class lists
+    newCard.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    imgContain.classList.add('img-container');
+
+    // content
+    headline.textContent = data.headline;
+    image.src = data.authorPhoto;
+    authName.textContent = `By: ${data.authorName}`;
+
+    // appending
+    newCard.appendChild(headline);
+    newCard.appendChild(author);
+    author.appendChild(imgContain);
+    imgContain.appendChild(image);
+    author.appendChild(authName);
+
+    return newCard;
+}
 
 
